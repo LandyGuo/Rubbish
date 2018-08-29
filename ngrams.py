3e7b6500aa4bd45fb70e6bd751e62c13815d9472
Matrix1204 <gqp.hust@gmail.com>
2018-08-29 22:01:12 +0800

    增加2-4grams的支持

--- ngrams.py
+++ ngrams.py
@@ -3,6 +3,7 @@ import math
 import codecs
 import copy
 import json
+import sys
 
 from collections import defaultdict
 
@@ -10,8 +11,11 @@ from collections import defaultdict
 def count_ngram(lines, ngram):
     counter = {}
     for i, line in enumerate(lines):
-        # ngrams
-        for i in range(len(lines) - ngram + 1):
+        # add padding
+        padding_prefix = '$[&'[:ngram-1]
+        padding_end = padding_prefix[::-1].replace('[', ']')
+        line = padding_prefix + line + padding_end
+        for i in range(len(line) - ngram + 1):
             cur_n_gram = line[i:i + ngram]
             if cur_n_gram not in counter:
                 counter[cur_n_gram] = 0
@@ -26,16 +30,17 @@ def load_txt(corpus_file):
         line = line.strip()
         if not line:
             continue
+        # if ln >= 2000: break
         lines.append(line)
     return lines
 
 
 class NGramModel(object):
 
-    def __init__(self, ngrams=2, model=None):
+    def __init__(self, ngrams=[], model=None):
+        assert ngrams, 'ngrams should be a list, eg [2], [2,3], [3,4] etc'
         self.ngrams = ngrams
         self.params = {}
-        self.debug = False
         if model:
             self.load(model)
 
@@ -54,42 +59,73 @@ class NGramModel(object):
         self.params = json.load(open(model_file, 'rb'))
 
     def train(self, corpus, save_model):
-        word_prob = count_ngram(corpus, 1)
-        ngram_prob = count_ngram(corpus, self.ngrams)
-        word_prob.sort(key=lambda x: x[1], reverse=True)
-        word_cnt = dict(word_prob)
-        self.word_size = len(word_prob)
+        # 建立ngram词表
+        ngram_freq, word_freq = {}, []
+        for ngram in range(1, max(self.ngrams)+1):
+            ngram_freq[ngram] = count_ngram(corpus, ngram)
+            word_freq+=ngram_freq[ngram] # 合并ngram词表
+
+        # 按照出现频次倒排
+        word_freq.sort(key=lambda x: x[1], reverse=True)
+
+        # 去掉出现频次5次以下的词
+        word_freq = [x for x in word_freq if x[1]>=5]
+
+        self.word_size = len(word_freq)
+
+        # 建立词和id之间的映射
         self.id2word = dict([(id, word[0])
-                             for id, word in zip(range(self.word_size), word_prob)])
+                             for id, word in zip(range(self.word_size), word_freq)])
         self.word2id = dict(map(lambda x: x[::-1], self.id2word.items()))
-        self.word_prob_map = dict(
-            [(k, math.log((1.0 * v + 1.0) / (self.word_size + 1.0))) for k, v in word_prob])
+
+        # 计算bigram prob
         self.prob_matrix = defaultdict(dict)
 
-        for k, v in ngram_prob:
-            if len(k) != 2: continue
-            k0, k1 = self.word2id.get(
-                k[0], self.word_size), self.word2id.get(
-                k[1], self.word_size)
-            self.prob_matrix[str(k0)][str(k1)] = math.log( (v * 1.0 + 1.0) / (word_cnt.get(self.id2word.get(k0, '#'),0) + self.word_size))
+        for ngram in self.ngrams:
+            assert ngram >= 2, 'ngram should gt 2!, but got {} instead'.format(ngram)
+            n_freq = dict(ngram_freq[ngram])
+            n_1_freq = dict(ngram_freq[ngram-1])
+
+            for k in n_freq.keys():
+                if len(k)!=ngram: continue
+                id0, id1 = self.word2id.get(
+                    k[:ngram-1], self.word_size), self.word2id.get(
+                    k[-1], self.word_size)
+                # k0k1出现次数/k0出现次数
+                k0_cnt = n_1_freq.get(k[:ngram-1], 0)
+                k0k1_cnt = n_freq.get(k, 0)
+                self.prob_matrix[str(id0)][str(id1)] = math.log((k0k1_cnt * 1.0 + 1.0) / (k0_cnt + len(n_1_freq)))
 
         self.save(word_size=self.word_size,
                   word2id=self.word2id,
                   prob_matrix=self.prob_matrix,
-                  word_prob_map=self.word_prob_map)
+                  ngrams = self.ngrams)
         self.dump(save_model)
 
+
     def predict(self, sent):
-        sent = sent.strip()
-        p = self.params['word_prob_map'].get(
-            sent[0], math.log(1.0 / self.params['word_size'] + 1))
-        if len(sent) <= 1:
-            return p
-        for i in range(1, len(sent)):
-            cond_p = self.condition_prob(sent[i - 1], sent[i])
-            p += cond_p
-        avg_p = p / (len(sent) - 1)
-        return avg_p
+        ori_sent = sent.strip()
+
+        # padding sentence
+        prob, normal = 0, 0
+        # print("sent:%s"%sent.encode('utf-8'))
+        for ngram in self.params['ngrams']:
+            padding_prefix = '$[&'[:ngram - 1]
+            padding_end = padding_prefix[::-1].replace('[', ']')
+            sent = padding_prefix + ori_sent + padding_end
+            
+
+            # print("-------------------------ngrams:%s-----------------------"%ngram)
+            # print("sentence:%s"%sent)
+            p = 0
+            for i in range(ngram-1, len(sent)):
+                cond_p = self.condition_prob(sent[i-ngram+1:i], sent[i])
+                # print("{} gram: {}->{}:{}".format(ngram, sent[i-ngram+1:i].encode('utf-8'), sent[i].encode('utf-8'), cond_p))
+                p += cond_p
+            prob += p
+            normal += len(sent) - ngram+1
+        return prob*1.0/normal
+
 
     def condition_prob(self, x, y):  # p(y/x)
         x_id, y_id = self.params['word2id'].get(
@@ -98,11 +134,15 @@ class NGramModel(object):
         return self.params['prob_matrix'].get(str(x_id), {}).get(str(y_id), -20)
 
 
-if __name__ == "__main__":
-    lm = NGramModel(model='hospital.2grams.model')
 
-    test_cases = [u'test1', u'test2', u'test3']
+if __name__ == "__main__":
+    corpus_path = sys.argv[1]
+    corpus = load_txt(corpus_path)
+    lm = NGramModel(ngrams=[2,3,4], model=None)
+    lm.train(corpus, 'hospital.234gram.20180828')
+
+    test_cases = [u'收费专用章', u'楼番禺区妇药团*药', u'医*第天', u'宁波市城镇职工基本医疗', u'综合医院外科门诊', u'门港医专疗疗诊费发票',u'沈阳皇姑国防医院',u'桂号票山天发**']
     for case in test_cases:
         score = lm.predict(case)
-        pred = 1 if score < -10.0 else 0  
+        pred = 1 if score < -10.0 else 0  # 1 有问题的医院名称 ； 0  正常医院名称
         print("case:%s score:%s result:%s" % (case, score, pred))
