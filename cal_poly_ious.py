95eea3db01922720ddc1350c4b6ecca51311af00
Matrix1204 <gqp.hust@gmail.com>
2019-05-06 13:27:44 +0800

    fix poly-iou bug

--- cal_poly_ious.py
+++ cal_poly_ious.py
@@ -1,11 +1,10 @@
 #coding=utf-8
 import cv2
 import numpy as np
-import time
 
 
-# boxes: N x k x 2
-# boxes: M x k x 2,
+# poly_arr1: N x k x 2
+# poly_arr2: M x k x 2,
 def cal_iou(poly_arr1, poly_arr2):
     poly1, poly2 = np.array(poly_arr1, dtype=np.int32), np.array(poly_arr2, dtype=np.int32)
     N, M = poly1.shape[0], poly2.shape[0]
@@ -27,13 +26,15 @@ def cal_iou(poly_arr1, poly_arr2):
         poly2_masks.append(mask.reshape((-1,)))
     poly1_masks, poly2_masks = np.array(poly1_masks, dtype=np.float32), np.array(poly2_masks, dtype=np.float32)
     inter = np.dot(poly1_masks, poly2_masks.T) # N*M
-    area1, area2 = np.sum(poly1_masks, axis=1), np.sum(poly2_masks, axis=1) # M, N
-    area1, area2 = np.tile(area1, [N, 1]), np.tile(area2, [M, 1])
+    area1, area2 = np.sum(poly1_masks, axis=1), np.sum(poly2_masks, axis=1) # N, M
+    area1, area2 = np.tile(area1.reshape((N, 1)), [1, M]), np.tile(area2.reshape((1,M)), [N, 1])
     union = (area1 + area2-inter).astype(np.float32)
     return inter/union
 
 if __name__=="__main__":
     image = np.zeros((100, 100), dtype='uint8')
-    box1 = np.array([[0, 0], [49, 0], [49, 49], [0, 49]], dtype=np.int32)
-    box2 = np.array([[0, 0], [49, 0], [49, 49], [99, 49], [99, 99], [0, 99]], dtype=np.int32)
-    print(cal_iou([box1], [box2]))
+    box1 = np.array([[0, 0], [49, 0], [49, 49], [0, 49]], dtype=np.int32) # 4x2
+    box2 = np.array([[0, 0], [49, 0], [49, 99], [0, 99]], dtype=np.int32) # 6x2
+    boxa = np.stack([box1, box2], axis=0) # 2x4x2
+    boxb = np.stack([box2, box1, box2], axis=0) # 3x4x2
+    print(cal_iou(boxa, boxb))
